af000a565b329cf217638c9b2bca669457a7afba
Juan Antonio Peñalver Rodríguez <juancompositor@gmail.com>
2025-06-27 12:37:34 +0100

    Preparación del proyecto de la web de Mónica. Preparando la estructura de carpetas necesaria.

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Página principal
+        main: resolve(__dirname, 'index.html'),
+
+        // Páginas secundarias (activarlas cuando las tengamos listas)
+        // ejemplo: resolve(__dirname, 'html/ejemplo.html'),
+        // curriculum: resolve(__dirname, 'html/curriculum.html'),
+        // contacto: resolve(__dirname, 'html/contacto.html'),
+      },
+    },
+  },
+});
