4ef71b850069b42b1f60c39ac23e56fd84b54b11
Juan Antonio Peñalver Rodríguez <juancompositor@gmail.com>
2025-07-02 17:24:04 +0100

    Instalación de swiper con npm y creación de slider.js con el código necesario para que funcionen los carruseles de imágenes.

---
+++ src/js/slider.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+export function initSlider(selector, customConfig = {}) {
+    const defaultConfig = {
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    };
+
+    return new Swiper(selector, { ...defaultConfig, ...customConfig });
+}
