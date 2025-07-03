fe6262ca375e74a0e53e2375edf693d42d11583b
Juan Antonio Peñalver Rodríguez <juancompositor@gmail.com>
2025-07-03 12:00:00 +0100

    Arreglados pequeños fallos. Hero apartenemente terminado.

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,16 @@ import './menu.js';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+// Importo initSlider
+import { initSlider } from './slider.js';
+
 // Inicializa animaciones con AOS (Animate On Scroll)
 AOS.init({
   duration: 800, // duración de la animación en ms
   once: true     // solo se ejecuta una vez al hacer scroll
 });
+
+// Inicializa la función que ejecuta el swiper (que está en slider.js)
+if (document.querySelector('.swiper')) {
+  initSlider('.swiper');
+}
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,22 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 
-Swiper.use([Navigation, Pagination, Autoplay]);
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 
 export function initSlider(selector, customConfig = {}) {
     const defaultConfig = {
-        loop: true,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true // para que la transición sea de fundido cruzado
+        },
+        speed: 4000, // duración de la transición en ms (4 segundos)
+        // loop: true,  // Cuando lo activo, me da problemas al hacer clic.
         autoplay: {
-            delay: 5000,
+            delay: 4000,    // tiempo que la imagen está visible
             disableOnInteraction: false,
         },
         navigation: {
