4f2f1e411f9f0de0e82c80d853cbef3771cd4be3
Juan Antonio Peñalver Rodríguez <juancompositor@gmail.com>
2025-08-17 13:17:43 +0200

    Cambiado el color de fondo de las .tarjeta-servicio y cambiada la configuración de vite-config.js para poder abrir el servidor desde dispositivos externos

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
   build: {
     rollupOptions: {
       input: {
