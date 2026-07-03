476b6a35cca7d61e0b2f60574ba724b7aab3f0b3
Juan Antonio Peñalver Rodríguez <juancompositor@gmail.com>
2026-07-03 14:54:56 +0200

    añadidos los og-meta de los header, añadida la pagina 404 y los estilos de la misma

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,16 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
 
         // Páginas secundarias (activarlas cuando las tengamos listas)
+        avisoLegal: resolve(__dirname, 'html/aviso-legal.html'),
+        colonoscopia: resolve(__dirname, 'html/colonoscopia.html'),
+        consultaPresencial: resolve(__dirname, 'html/consulta-presencial.html'),
+        ecografia: resolve(__dirname, 'html/ecografia.html'),
+        gastroscopia: resolve(__dirname, 'html/gastroscopia.html'),
+        politicaCookies: resolve(__dirname, 'html/politica-cookies.html'),
+        politicaPrivacidad: resolve(__dirname, 'html/politica-privacidad.html'),
+        posts: resolve(__dirname, 'html/posts.html'),
+        sobreMi: resolve(__dirname, 'html/sobre-mi.html'),
         videoconsulta: resolve(__dirname, 'html/videoconsulta.html'),
-        // ejemplo: resolve(__dirname, 'html/ejemplo.html'),
-        // contacto: resolve(__dirname, 'html/contacto.html'),
       },
     },
   },
